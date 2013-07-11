7fe1d71b585a5bbe5a4ca86ae7fee523c658a1af
Gilles Devaux <gilles.devaux@gmail.com>
2013-07-11 16:40:15 -0700

    make sure the data dir exists

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,11 +1,18 @@
 include_recipe "cassandra::datastax"
 
+[node[:cassandra][:data_root_dir], node[:cassandra][:commitlog_dir]].each do |dir|
+  directory dir do
+    owner node[:cassandra][:user]
+    group node[:cassandra][:user]
+  end
+end
+
 %w(cassandra.yaml cassandra-env.sh).each do |f|
   template File.join(node["cassandra"]["conf_dir"], f) do
     source "#{f}.erb"
     owner node["cassandra"]["user"]
     group node["cassandra"]["user"]
-    mode  0644
+    mode 0644
     notifies :restart, "service[cassandra]"
   end
 end
