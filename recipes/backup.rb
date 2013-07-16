7a46bc2a35e8fb4f8f8f2238e64e9fd35c42218c
tonyjchong <tony.chong@kwarter.com>
2013-07-16 12:39:24 -0700

    fixed the backup script

--- recipes/backup.rb
+++ recipes/backup.rb
@@ -16,8 +16,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-cookbook_file "#{node[:cassandra][:cassandra_backup][:backup_path]}/cassandra_backup.sh" do
-  source "cassandra_backup.sh"
+template "#{node[:cassandra][:cassandra_backup][:backup_path]}/cassandra_backup.sh" do
+  source "cassandra_backup.sh.erb"
   mode 0755
 end
 
